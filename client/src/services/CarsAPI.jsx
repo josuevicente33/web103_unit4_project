c42ff447ed81a1bc729da97e9330f6bbdb19b451
Josue Vicente <70499804+josuevicente33@users.noreply.github.com>
2025-10-23 14:47:27 -0400

    Create CarsAPI.jsx

---
+++ client/src/services/CarsAPI.jsx
@@ -0,0 +1,9 @@
+// API for cars
+export const getAllCars = async () => {
+    const response = await fetch('/api/cars');
+    return response.json();
+}
+export const getCarById = async (id) => {
+    const response = await fetch(`/api/cars/${id}`);
+    return response.json();
+}
